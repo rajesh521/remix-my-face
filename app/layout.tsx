0a5e814a0f3041a858662b12e08232e2180e7996
rajesh <61110612+rajesh521@users.noreply.github.com>
2023-06-13 18:12:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Remix My Face | powered by Leap",
+  title: "Remix My Face | powered by Rajesh",
   description: "Take a selfie, get a custom avatar.",
 };
 
